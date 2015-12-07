233acb6d7b9f3c8625481a6ed6c26385949bae2f
anhpnguyen.an@gmail.com <anhpnguyen.an@gmail.com>
2015-12-07 16:51:10 -0500

    added final project

---
+++ final/js/progress.js
@@ -0,0 +1,11 @@
+//Progress bar
+$(window).scroll(function () {
+  var s = $(window).scrollTop(),
+        d = $(document).height(),
+        c = $(window).height();
+        scrollPercent = (s / (d-c)) * 100;
+        var position = scrollPercent;
+
+   $("#progressbar").attr('value', position);
+
+});
---
+++ final/js/smoothscroll.js
@@ -0,0 +1,7 @@
+//Smooth scrolling
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
